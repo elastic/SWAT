c96097448aa9e856338682aec14082ddddfc7ec2
brokensound77 <brokensound77@users.noreply.github.com>
2023-07-29 19:35:19 -0600

    add cred_store methods

--- swat/base.py
+++ swat/base.py
@@ -20,6 +20,14 @@ class SWAT:
     creds: Optional[Credentials] = field(default=None)
     cred_store: Optional[CredStore] = field(default_factory=CredStore)
 
-    def add_creds(self, key, value):
+    def add_cred(self, key, value):
         """Add a credential to the store."""
         setattr(self.cred_store, key, value)
+
+    def remove_cred(self, key):
+        """Remove a credential from the store."""
+        delattr(self.cred_store, key)
+
+    def get_creds(self) -> dict:
+        """Get the credentials from the store."""
+        return self.cred_store.__dict__
