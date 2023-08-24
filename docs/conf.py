ed6cbe24e14cd2bb826470d286da5013c6c63c04
terrancedejesus <terrance.dejesus@elastic.co>
2023-08-24 18:06:41 -0400

    added rtd theme; adding developers guide

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'terrance.dejesus@elastic.co,justin.ibarra@elastic.co'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +25,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
